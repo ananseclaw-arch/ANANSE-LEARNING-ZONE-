f2bc3936f7b8e7545c55de029ee3fbf90270fa79
Ananse Claw <ananseclaw@mac.lan>
2026-06-28 22:28:16 -0400

    Switch TTS to StreamElements API + SW claim() fix, cache v7

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE = "learning-zone-v6";
+const CACHE = "learning-zone-v7";
 const FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon-180.png", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
 });
 self.addEventListener("activate", e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+  e.waitUntil(clients.claim().then(() => caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
 });
 self.addEventListener("fetch", e => {
   e.respondWith(caches.match(e.request, {ignoreSearch: true}).then(r => r || fetch(e.request).then(resp => {
